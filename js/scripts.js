36b779fab19391dc25a3100d4d5e688d4628ba5e
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-01-16 02:01:01 -0500

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-document.getElementById("textbox")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("startButton").click();
-    }
-});
+function begin() { 
+    var textbox = document.getElementById("inbox").value; 
+    console.log(textbox); //test
 
-var textbox = document.getElementById("inbox").value; 
-console.log(textbox);
+}
+
+function findNext() {
+    
+}
