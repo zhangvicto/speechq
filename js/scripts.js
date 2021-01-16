cb5da31ec3903ce6aa189f3dfc51be08196aa686
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-01-16 12:42:00 -0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,32 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var recognition = new SpeechRecognition();
+    var SpeechRecognitionList = new SpeechGrammarList();
+    SpeechRecognitionList.addFromString(grammar, 1);
+    recognition.grammars = SpeechRecognitionList;
+
+    recognition.continuous = false;
+    recognition.lang='en-US';
+    recognition.interrimResults = false;
+    recognition.maxAlternatives = 1;
+
+
 function begin() { 
     var textbox = document.getElementById("inbox").value; 
     console.log(textbox); //test
+    var str = textbox.split(" ");
+
+    //print the array on html
+    for(let i=0;i<str.length;i++) {
+        document.getElementById(output).innerHTML= str[i];
+    }
+
+    recognition.start();
 
 }
 
-function findNext() {
-    
+function nextWord() {
+
 }
