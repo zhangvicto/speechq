561bfadb9a091b31ed0bd2fe4153a46f4144de29
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-01-16 14:15:27 -0500

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ if (!('webkitSpeechRecognition' in window)) {
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
+}
 
 function begin() { 
     var textbox = document.getElementById("inbox").value; 
@@ -19,12 +20,33 @@ function begin() {
     //    document.getElementById("output").innerHTML= str[i];
     //}
 
-    recognition.lang = select_dialect.value;
+    recognition.lang = 'en-US';
     recognition.start();
-    
+    listen(str);
+
     //need some condition and then nextWord(); //find next word
 }
 
-function nextWord() {
+function listen(str) {
+    recognition.onresult = function(event) {
+    var interim_transcript = '';
 
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+        if (interim_transcript = str[0]) {
+            startQ(str);
+        }
+      }
+    }
+    final_transcript = capitalize(final_transcript);
+    final_span.innerHTML = linebreak(final_transcript);
+    interim_span.innerHTML = linebreak(interim_transcript);
+  };
+}
+
+function startQ(str) {
+    
 }
